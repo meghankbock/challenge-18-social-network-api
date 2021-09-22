5d2ba7dabc8948dbb3fb4cd5b0dff6b78e3ecbe7
Meghan Bock <meghan.bock@gmail.com>
2021-09-22 06:02:12 -0500

    set up user model

---
+++ models/User.js
@@ -0,0 +1,53 @@
+const { Schema, model } = require("mongoose");
+import { isEmail } from 'validator';
+
+const UserSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true
+        },
+        
+        email: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true,
+            validate: [ isEmail, 'Please provide valid email address.' ]
+        },
+
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "Thought"
+            }
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "User"
+            }
+        ]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false,
+    }
+);
+
+UserSchema.virtual("thoughtCount").get(function () {
+    return this.thoughts.length;
+});
+
+UserSchema.virtual("friendCount").get(function () {
+    return this.friends.length;
+});
+
+const User = model("User", UserSchema);
+
+module.exports = User;
