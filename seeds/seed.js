7722bdd75a8f65f328513253c2e84b2b45514fc4
Meghan Bock <meghan.bock@gmail.com>
2021-10-10 09:51:41 -0500

    fixes to seed.js file

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,35 +1,62 @@
 const faker = require("faker");
-
-const db = require("./connection");
+const mongoose = require('mongoose');
 const { User, Thought } = require("../models");
 
-db.once("open", async () => {
-  console.log("seed start");
+const createSeeds = async () => {
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
   await User.deleteMany({});
   await Thought.deleteMany({});
+
+  const numUsers = 50;
 
   // create user data
   const userData = [];
 
-  for (let i = 0; i < 50; i += 1) {
+  for (let i = 0; i < numUsers; i += 1) {
     const username = faker.internet.userName();
     const email = faker.internet.email(username);
+    let { _id } = faker.datatype.hexaDecimal(22);
 
-    userData.push({ username, email });
+    userData.push({ _id, username, email });
   }
+  console.log("userData: ", userData);
 
   const createdUsers = await User.collection.insertMany(userData);
+
+  // create friends
+  for (let i = 0; i < userData.length; i++) {
+    const randomUserIndex = Math.floor(Math.random() * userData.length);
+    const userId = userData[randomUserIndex]._id;
+
+    if (userData[i]._id !== userId) {
+        await User.findOneAndUpdate(
+            { _id: userData[i]._id },
+            { $push: { friends: userId } },
+            { new: true }
+        )
+    };
+
+    console.log("userData: ", userData);
+  }
 
   // create thought data
   let createdThoughts = [];
   const thoughtText = faker.lorem.sentence();
 
-  const randomUserIndex = Math.floor(Math.random() * createdUsers.ops.length);
-  const { _id: userId, username } = createdUsers.ops[randomUserIndex];
+  const randomUserIndex = Math.floor(Math.random() * userData.length);
+  const userId = userData[randomUserIndex]._id;
+  const username = userData[randomUserIndex].username;
+  const { _id } = faker.datatype.hexaDecimal(22);
 
     const createdThought = await Thought.create({
       thoughtText,
-      username
+      username,
+      _id
     });
     const updatedUser = await User.updateOne(
       { _id: userId },
@@ -41,11 +68,11 @@ db.once("open", async () => {
   for (let i = 0; i < 100; i += 1) {
     const reactionBody = faker.lorem.sentence();
 
-    const randomUserIndex = Math.floor(Math.random() * createdUsers.ops.length);
-    const { _id: userId, username } = createdUsers.ops[randomUserIndex];
+    const randomUserIndex = Math.floor(Math.random() * userData.length);
+    const username = userData[randomUserIndex].username;
 
     const randomThoughtIndex = Math.floor(Math.random() * createdThoughts.length);
-    const { _id: thoughtId } = createdThoughts[randomThoughtIndex];
+    const thoughtId = createdThoughts[randomThoughtIndex]._id;
 
     await Thought.updateOne(
       { _id: thoughtId },
@@ -56,4 +83,6 @@ db.once("open", async () => {
 
   console.log("all done!");
   process.exit(0);
-});
+};
+
+createSeeds();
