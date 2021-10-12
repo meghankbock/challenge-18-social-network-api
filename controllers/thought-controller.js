46c524ed7ddd3c8b1c44c533ae0c88c6a4c0f073
Meghan Bock <meghan.bock@gmail.com>
2021-10-12 01:07:53 -0500

    remove unecessary returned field on thought

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -5,10 +5,8 @@ const thoughtController = {
   getAllThoughts(req, res) {
     Thought.find({})
       .populate({
-        path: "reactions",
-        select: "-__v",
+        path: "reactions"
       })
-      .select("-__v")
       .sort({ _id: -1 })
       .then((dbThoughtData) => res.json(dbThoughtData))
       .catch((err) => {
@@ -21,10 +19,8 @@ const thoughtController = {
   getThoughtById({ params }, res) {
     Thought.findOne({ _id: params.thoughtId })
       .populate({
-        path: "reactions",
-        select: "-__v",
+        path: "reactions"
       })
-      .select("-__v")
       .then((dbThoughtData) => {
         if (!dbThoughtData) {
           res.status(404).json({ message: "No thought found with this id!" });
